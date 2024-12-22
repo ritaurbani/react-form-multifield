cbcb737a1e79d066c9d64001d9b64d5d61fbcc92
ritaurbani <ritaurbani@gmail.com>
2024-12-22 23:35:04 +0100

    feat: added checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 
-
-
 function App() {
 
   const initialFormData = {
@@ -9,62 +7,54 @@ function App() {
     content: "",
     category: "",
     image: "https://picsum.photos/400/300",
-    published: true
+    published: false,
+    tags: [],
   }
 
   const [posts, setPosts] = useState([])
 
   const [formData, setFormData] = useState(initialFormData)// object
 
-  // FUNZIONE UNICA PER GESTIRE EVENTO ONCHANGE-Recuperiamo nome e valore del campo-Aggiorniamo il nostro stato
-  function handleEventOnChange(event) {
-    setFormData((formData) => ({
-      ...formData,
-      [event.target.name]: event.target.value,
-    })
-    )
-  }
+  //               FUNZIONE ONCHANGE
+  const handleEventOnChange = (event) => {
+    const keyToChange = event.target.name;
+    // Se l'input è checkbox,
+    //    allora il value da inserire sarà true o false, preso da target.checked
+    let newValue;
 
-  //Form function-Aggiornare lsita-Aggiungere oggetto alla lista
-  const handlePostForm = (event) => {
-    event.preventDefault();
-    // intercetta bottone - console.log("submit") 
-    // AGGIUNGIAMO NUOVO POST AL BLOG
-    setPosts((posts) => [...posts, { id: Date.now(), ...formData }]);
-    // RESETTO FORM E PORTO A STATO INIZIALE
-    setFormData(initialFormData);
+    if (event.target.type === "checkbox") {
+      newValue = event.target.checked;
+    } else {
+      newValue = event.target.value;
+    }
+
+    const newData = {
+      ...formData,
+      [keyToChange]: newValue,
+    };
+
+    setFormData(newData);
   };
 
-  //                    // olga
-  //   const handleEventOnChange = (event) => {
-  //     const keyToChange = event.target.name;
-  //     const newValue = event.target.value;
-  //   const newData = {
-  //     ...formData,
-  //     [keyToChange]: newValue,
-  //   };
+  //               FUNZIONE FORM SUBMIT
+  const handlePostForm = (event) => {
+    event.preventDefault();
 
-  //   setFormData(newData);
-  // };
+    // 1 creo l'oggetto del nuovo post
+    const newPost = {
+      ...formData,
+      id: Date.now(),
+    };
 
-  //   const handlePostForm = (event) => {
-  //     event.preventDefault();
+    // 2 creo la copia dell'array posts precedente, aggiungendo il nuovo post
+    const newArray = [...posts, newPost];
 
-  //     // 1 creo l'oggetto della nuova pizza
-  //     const newPost = {
-  //       ...formData,
-  //       id: Date.now(),
-  //     };
+    // 3. aggiorno lo stato dei posts
+    setPosts(newArray);
 
-  //     // 2 creo la copia dell'array menu precedente, aggiungendo la nuova pizza
-  //     const newArray = [...posts, newPost];
-
-  //     // 3. aggiorno lo stato del menu
-  //     setPosts(newArray);
-
-  //     // 4. Ripulisco i campi del form
-  //     setFormData(initialFormData);
-  //   };
+    // 4. Ripulisco i campi del form
+    setFormData(initialFormData);
+  };
 
   const removeElement = (elementToRemove) => {
     const newArray = posts.filter((curpost) => curpost.id !== elementToRemove);
@@ -78,7 +68,9 @@ function App() {
 
         {/* INPUT TITLE */}
         <section>
+          {/* //FORM */}
           <form onSubmit={handlePostForm} >
+
             <div className='mb-3'>
               <label htmlFor="titolo">Cerca titolo articolo</label>
               <input
@@ -89,6 +81,7 @@ function App() {
                 value={formData.title}
                 onChange={handleEventOnChange} />
             </div>
+
             {/* INPUT CONTENT */}
             <div className='mb-3'>
               <label htmlFor="content">Inserisci una descrizione</label>
@@ -100,6 +93,7 @@ function App() {
                 value={formData.content}
                 onChange={handleEventOnChange}></textarea>
             </div>
+
             {/* INPUT CATEGORY */}
             <div className='mb-3'>
               <label htmlFor="category">Category:</label>
@@ -110,13 +104,14 @@ function App() {
                 name='category'
                 value={formData.category}
                 onChange={handleEventOnChange}>
-                  <option value="">Select a category</option>
-                  <option value="technology">Fashion</option>
-                  <option value="science">Science</option>
-                  <option value="art">Art</option>
-                  <option value="history">History</option>
+                <option value="">Select a category</option>
+                <option value="technology">Fashion</option>
+                <option value="science">Science</option>
+                <option value="art">Art</option>
+                <option value="history">History</option>
               </select>
             </div>
+
             {/* INPUT IMAGE */}
             <div className='mb-3'>
               <label htmlFor="content">Image</label>
@@ -126,9 +121,32 @@ function App() {
                 id='image'
                 name='image'
                 value={formData.image}
-                onChange={handleEventOnChange}></input>
+                onChange={handleEventOnChange}/>
             </div>
-            
+
+            {/* INPUT PUBLISHED */}
+            <div className='mb-3'>
+              <label htmlFor="published">Published</label>
+              <input
+                type="checkbox"
+                id='published'
+                name='published'
+                checked={formData.published}
+                onChange={handleEventOnChange}/>
+            </div>
+
+            {/* INPUT TAGS */}
+            {/* <div className='mb-3'>
+              <label htmlFor="published">Published</label>
+              <input
+                type="checkbox"
+                id='published'
+                name='published'
+                checked={formData.published}
+                onChange={handleEventOnChange}/>
+            </div> */}
+
+            {/* //BUTTON */}
             <button type='submit' className='my-4 btn btn-success'>Submit</button>
           </form>
         </section>
@@ -144,11 +162,11 @@ function App() {
                     <h4>{post.title}</h4>
                     <p>{post.content}</p>
                     <p>{post.category}</p>
-                    <img src={post.image} alt="" 
-                      className='w-75 p-3'/>
-                      <div>
-                    <button className='mx-2 btn btn-outline-success btn-sm'
-                      onClick={() => { removeElement(post.id) }}>Cancel</button>
+                    <img src={post.image} alt=""
+                      className='w-75 p-3' />
+                    <div>
+                      <button className='mx-2 btn btn-outline-success btn-sm'
+                        onClick={() => { removeElement(post.id) }}>Cancel</button>
                     </div>
                   </div>
                 </div>
